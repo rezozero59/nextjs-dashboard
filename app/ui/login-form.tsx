58fdb87858da5722865c7879916f4ef7b365106f
rezozero59 <laurentdelassus@gmail.com>
2023-12-27 10:16:33 +0100

    traduction

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -17,7 +17,7 @@ export default function LoginForm() {
     <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
+          Connectez-vous pour continuer
         </h1>
         <div className="w-full">
           <div>
@@ -44,7 +44,7 @@ export default function LoginForm() {
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
               htmlFor="password"
             >
-              Password
+              Mot de passe
             </label>
             <div className="relative">
               <input
@@ -82,7 +82,7 @@ function LoginButton() {
   const { pending } = useFormStatus();
   return (
     <Button className="mt-4 w-full" aria-disabled={pending}>
-      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+      Connexion <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   );
 }
